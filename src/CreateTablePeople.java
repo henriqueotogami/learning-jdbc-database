bc38dc7f785a8ec67340d328cecd448e9e655fdb
HenriqueMAP <henrique.map@outlook.com>
2022-08-22 23:51:07 -0300

    Lecture 312 - Inserting new person into the database using SQL injection handling.

--- src/CreateTablePeople.java
+++ src/CreateTablePeople.java
@@ -2,7 +2,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class createTablePeople {
+public class CreateTablePeople {
 
     public static void main(String[] args) throws SQLException {
 
---
+++ src/NewPerson.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class NewPerson {
+
+    public static void main(String[] args) throws SQLException {
+
+        Scanner inputTypedUser = new Scanner(System.in);
+
+        System.out.println("Enter the name: ");
+        final String personName = inputTypedUser.next();
+
+        Connection factoryConnection = FactoryConnection.getConnection();
+        final String sqlCommand = "INSERT INTO people (person_name) VALUES (?)";
+        PreparedStatement factoryPreparedStatement = factoryConnection.prepareStatement(sqlCommand);
+        factoryPreparedStatement.setString(1, personName);
+        factoryPreparedStatement.setInt(2,100);
+        factoryPreparedStatement.execute();
+
+        System.out.println("Person successfully added.");
+        inputTypedUser.close();
+    }
+}
